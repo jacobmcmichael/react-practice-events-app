09a34153672d2380cd9fe565a5184f6088a2cea7
jacobmcmichael <jacobmcmichael@gmail.com>
2024-12-30 14:43:54 -0800

    :wrench: adds accent color to tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -6,7 +5,7 @@ export default function Home() {
 		<main className="flex flex-col items-center px-3 pt-36">
 			<h1 className="text-3xl lg:text-6xl font-bold tracking-tight">Find events around you</h1>
 			<p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
-				Browse more than <span className="font-bold text-[#a4f839] italic underline">10,000</span> events around
+				Browse more than <span className="font-bold text-accent italic underline">10,000</span> events around
 				you
 			</p>
 
@@ -15,7 +14,7 @@ export default function Home() {
 				action=""
 			>
 				<input
-					className="w-full h-16 rounded-lg bg-white/[7%] focus:bg-white/10 px-6 outline-none ring-[#a4f839]/50 focus:ring-2 transition"
+					className="w-full h-16 rounded-lg bg-white/[7%] focus:bg-white/10 px-6 outline-none ring-accent/50 focus:ring-2 transition"
 					type="text"
 					placeholder="Search events in any city..."
 					spellCheck={false}
