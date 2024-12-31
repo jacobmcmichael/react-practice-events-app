3d843524b918b6d4ba5b6e3c2a912baa8ac7c806
jacobmcmichael <jacobmcmichael@gmail.com>
2024-12-31 12:24:56 -0800

    :art: adds styles for nav links

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,16 @@
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Logo() {
 	return (
-		<Image
-			src="/portrait.jpg"
-			alt="Logo"
-			width={100}
-			height={100}
-		/>
+		<Link href="/">
+			<Image
+				className="brightness-0 invert opacity-75 hover:opacity-100 transition"
+				src="/logo.svg"
+				alt="Logo"
+				width={100}
+				height={100}
+			/>
+		</Link>
 	);
 }
