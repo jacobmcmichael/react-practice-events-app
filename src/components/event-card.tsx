cd273e6f140407bf3cd1b3e09c2a6fc2220c804d
jacobmcmichael <jacobmcmichael@gmail.com>
2024-12-31 17:53:00 -0800

    :feat: adds framer motion animations to events list

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,12 +1,31 @@
+"use client";
+
+import { useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+
 import { EventifierEventType } from "@/lib/types";
 
+const MotionLink = motion(Link);
+
 export default function EventCard({ event }: { event: EventifierEventType }) {
+	const ref = useRef(null);
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ["0 1", "1.5 1"],
+	});
+	const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+	const scaleOpacity = useTransform(scrollYProgress, [0, 1], [0.3, 1]);
+
 	return (
-		<Link
+		<MotionLink
+			ref={ref}
 			className="flex-1 basis-80 h-[380px] max-w-[500px]"
 			href={`/event/${event.slug}`}
+			// @ts-ignore
+			style={{ scale: scaleProgress, opacity: scaleOpacity }}
+			initial={{ scale: 0.8, opacity: 0 }}
 		>
 			<section className="w-full h-full relative flex flex-col bg-white/[3%] rounded-xl overflow-hidden state-effects">
 				<Image
@@ -32,6 +51,6 @@ export default function EventCard({ event }: { event: EventifierEventType }) {
 					</p>
 				</section>
 			</section>
-		</Link>
+		</MotionLink>
 	);
 }
