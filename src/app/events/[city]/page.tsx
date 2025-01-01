c93acd1d41d837706577176a05dacaa739da7578
jacobmcmichael <jacobmcmichael@gmail.com>
2024-12-31 17:30:08 -0800

    :feat: adds city name to window title metadata

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,4 +1,5 @@
 import { Suspense } from "react";
+import { Metadata } from "next";
 
 import { toCapitalCase } from "@/lib/utilities";
 import { EventsPageProps } from "@/lib/props";
@@ -6,6 +7,15 @@ import { EventsPageProps } from "@/lib/props";
 import H1 from "@/components/h1";
 import EventsList from "@/components/events-list";
 import Loading from "@/app/events/[city]/loading";
+
+export function generateMetadata({ params }: EventsPageProps): Metadata {
+	const city = params.city;
+
+	return {
+		title: city === "all" ? "All Events" : `Events in ${toCapitalCase(city)}`,
+		description: `Find events in ${toCapitalCase(city)} and enjoy your time with your friends and family.`,
+	};
+}
 
 export default async function EventsPage({ params }: EventsPageProps) {
 	const city = params.city;
